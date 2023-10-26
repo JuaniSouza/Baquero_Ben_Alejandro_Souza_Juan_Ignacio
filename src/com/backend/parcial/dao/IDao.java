7d3d9959db62a56c07e1f8c5caff075c9bd83b7f
JuaniSouza <juansouzacomercial@gmail.com>
2023-10-26 18:51:23 -0300

    Agregacion de SQL, H2Connection y DAO

---
+++ src/com/backend/parcial/dao/IDao.java
@@ -0,0 +1,8 @@
+package com.backend.parcial.dao;
+
+public interface IDao<T> {
+
+    //T registrar(T t);
+
+    //T buscarPorID(int id);
+}
--- src/com/backend/parcial/service/servicio.java
+++ src/com/backend/parcial/service/servicio.java
@@ -1,4 +1,5 @@
 package com.backend.parcial.service;
 
 public class servicio {
+    //capa de logica de negocio que interactua con la capa de persistencia, pero desconoce los detalles de la implementacion de esa persistencia
 }
